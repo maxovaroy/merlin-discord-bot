2efa1a29bfe92c174c014ea0d2c3b44738a41e7e
Maximo <maxovaroy@gmail.com>
2025-12-01 17:02:18 +0530

    Update profile_system.py

--- cogs/profile_system.py
+++ cogs/profile_system.py
@@ -57,7 +57,7 @@ class ProfileSystem(commands.Cog):
             return
 
         # Fetch or create profile
-        profile_data = self.storage.get_user_profile(target.id, ctx.guild.id)
+        profile_data = self.storage.get_user_profile(target.id, ctx.guild.id) or {}
 
         # XP & level
         user_levels = self.storage.user_levels.get(str(ctx.guild.id), {}).get(str(target.id), {})
@@ -207,3 +207,4 @@ async def setup(bot, storage=None):
     if storage is None:
         storage = getattr(bot, "storage", None)
     await bot.add_cog(ProfileSystem(bot, storage))
+
