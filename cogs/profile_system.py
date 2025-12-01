ac19e657e44a799bbaa64f1547ea5059a53869c6
Maximo <maxovaroy@gmail.com>
2025-12-01 17:03:35 +0530

    Update profile_system.py

--- cogs/profile_system.py
+++ cogs/profile_system.py
@@ -103,6 +103,8 @@ class ProfileSystem(commands.Cog):
         )
 
         # Stats
+        reputation = 0
+        if hasattr(self.storage, "get_reputation"):
         reputation = self.storage.get_reputation(target.id, ctx.guild.id)
         join_date = profile_data.get("created_at", str(ctx.author.created_at.date()))
         embed.add_field(
@@ -207,4 +209,5 @@ async def setup(bot, storage=None):
     if storage is None:
         storage = getattr(bot, "storage", None)
     await bot.add_cog(ProfileSystem(bot, storage))
+
 
